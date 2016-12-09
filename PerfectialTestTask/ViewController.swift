d590beec99e6d6ab7ac09d8c7d473237549f1b93
Dmytro Skorokhod <d.integralas@gmail.com>
2016-12-09 17:02:02 +0200

    Initial Commit

---
+++ PerfectialTestTask/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PerfectialTestTask
+//
+//  Created by Dmytro Skorokhod on 12/9/16.
+//  Copyright © 2016 Dima Skorokhod. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
